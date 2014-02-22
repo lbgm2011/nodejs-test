2065f459d5d3bee3e0a47dc2c798128b283ef8d7
Laoska Benyasca <lbgm2011@gmail.com>
2014-02-22 17:25:49 -0600

    hello world

--- benyasca/app.js
+++ benyasca/app.js
@@ -18,4 +18,4 @@ app.use(stylus.middleware({
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(req, res) { res.render('index', { title : 'Home'}) })
-app.listen(3000)
+app.listen(9009)
